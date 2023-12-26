4b2ad5f415c1a23b93ac70e1a2e052a6f51f6441
daniel4255 <danielulad1992@hotmail.com>
2023-12-26 18:54:52 -0500

    updated player

--- Player/player.gd
+++ Player/player.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-class_name player
+class_name Player
 
 
 func _process(delta):
--- State/PlayerMoveState.gd
+++ State/PlayerMoveState.gd
@@ -10,10 +10,10 @@ func Enter():
 	pass
 
 func state_process(delta: float):
-	_move_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
-
+	pass
 
 func state_physics_process(delta: float):
+	_move_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
 	player.velocity =  _move_dir * move_speed
 	player.move_and_slide()
 	
