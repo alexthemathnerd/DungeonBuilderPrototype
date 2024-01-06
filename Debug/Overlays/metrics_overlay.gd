1768057cc6d8d7a1f1e050f27c639e554d0cff92
Alexander Schmidt <aschmid3@my.westga.edu>
2024-01-06 13:35:09 -0500

    Changed Metrics Overlay

--- Debug/Overlays/metrics_overlay.gd
+++ Debug/Overlays/metrics_overlay.gd
@@ -1,9 +1,13 @@
 class_name MetricsOverlay
 extends PanelContainer
 
-@onready var fps_label = $ClientSection/Vbox/FPSLabel
+@onready var _fps_label = $ClientSection/Vbox/FPSLabel
+@onready var _frame_time_label = $ClientSection/Vbox/FrameTimeLabel
+
 
 func _process(_delta):
 	if visible:
-		var fps := Engine.get_frames_per_second()
-		fps_label.text = "FPS: {0}".format([fps])
+		var fps := Performance.get_monitor(Performance.TIME_FPS)
+		_fps_label.text = "FPS: {0}".format([fps])
+		var frame_time := Performance.get_monitor(Performance.TIME_PROCESS) * 1000
+		_frame_time_label.text = "Frame Time: {0} ms".format([frame_time])
